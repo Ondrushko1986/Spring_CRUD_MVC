78c9e8380523a7130c47fcd930c98c3739d83a9c
Ондрюшко <Ondrushko1986@yandex.ru>
2020-08-25 12:55:52 +0300

    Initial commit

---
+++ src/main/java/dao/UserDAO.java
@@ -0,0 +1,20 @@
+package dao;
+
+import model.User;
+
+import java.util.List;
+
+public interface UserDAO {
+
+        List<User> allUsers();
+
+        void add(User user);
+
+        void delete(User user);
+
+        void edit(User user);
+
+        User getById(int id);
+
+    }
+
